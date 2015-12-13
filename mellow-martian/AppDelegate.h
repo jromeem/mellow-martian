a58163884cf71d305d89e0612f44614b0acda1fd
Jerome Martinez <jerome.e.martinez@gmail.com>
2015-12-12 22:04:39 -0600

    initial commit

---
+++ mellow-martian/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  mellow-martian
+//
+//  Created by Jerome Martinez on 12/12/15.
+//  Copyright © 2015 TwoSyllables. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ mellow-martian/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  mellow-martian
+//
+//  Created by Jerome Martinez on 12/12/15.
+//  Copyright © 2015 TwoSyllables. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
